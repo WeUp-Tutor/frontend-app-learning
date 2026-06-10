dfa12cec2c3799818bd1ed9869016e1bed6fe094
theogicquel-weup <theo.gicquel@weuplearning.com>
2026-06-10 02:09:10 +0200

    remove bookmark button

--- src/plugin-slots/UnitTitleSlot/index.jsx
+++ src/plugin-slots/UnitTitleSlot/index.jsx
@@ -31,11 +31,7 @@ const UnitTitleSlot = ({
         {renderUnitNavigation(true)}
       </div>
       <p className="sr-only">{formatMessage(messages.headerPlaceholder)}</p>
-      <BookmarkButton
-        unitId={unit.id}
-        isBookmarked={unit.bookmarked}
-        isProcessing={isProcessing}
-      />
+
     </PluginSlot>
   );
 };
