71f28cbd9de6b9c2325766c678fdf8c48ec5b5d4
theogicquel-weup <theo.gicquel@weuplearning.com>
2026-06-12 05:26:15 +0200

    hardcode for now, see github issue https://github.com/openedx/frontend-app-learning/issues/1834

--- src/course-tabs/CourseTabLink.tsx
+++ src/course-tabs/CourseTabLink.tsx
@@ -15,6 +15,6 @@ export const CourseTabLink = ({
     href={url}
     className={classNames('nav-item flex-shrink-0 nav-link', { active: slug === activeTabSlug })}
   >
-    {title}
+    {title === 'cours' ? 'Parcours' : title}
   </a>
 );
